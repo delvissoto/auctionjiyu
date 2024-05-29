bc726f6f3ce6c74be1068532993a854fe084656e
delvissoto <delvissoto99@icloud.com>
2024-05-28 23:26:42 -0500

    logo and navbar created. react-app desktop display only.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import NoPage from './components/NoPage';
+import Login from './components/Login';
+import Sell from './components/Sell';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Navbar/>}>
+          <Route index element={<Home/>} />
+          <Route path="*" element={<NoPage />} />
+          <Route path='log_in' element={<Login/>}/>
+          <Route path='Sell' element={<Sell/>}/>
+          </Route>
+          
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+import logo from '../images/auctionjiyu.png'
+
+
+const Navbar = () => {
+  return (
+    <>
+    <nav className='navbar'>
+    <Link to="/"><img id='logo' src={logo} alt='Logo'/></Link>
+        <ul className='navsec1'>
+            <li className='navLinks'><Link to='/'>Auction</Link></li>
+            <li className='navLinks'><Link to='/sell'>Sell</Link></li>
+            <li className='navLinks'><Link to='/shipping'>Shipping</Link></li>
+            <li className='navLinks'><Link to='/about'>About</Link></li>
+            <li className='navLinks'><Link to='/my_profile'>Profile</Link></li>
+            
+        </ul>
+        <ul  className='navsec2'>
+        <li className='navLinks'><Link to='/log_in'>LogIn</Link> / <Link>SignUp</Link></li>
+        <input type='search' placeholder='Search'/>
+        </ul>
+    </nav>
+    <Outlet/>
+    </>
+  )
+}
+
+export default Navbar
